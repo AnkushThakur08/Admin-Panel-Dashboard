3948b0e23fb53bdbe29c8795d85a6c8a1a0c5fb5
Ankush Thakur <atpara99@gmail.com>
2022-12-20 11:09:51 +0530

    Final code Updated

--- src/backend.js
+++ src/backend.js
@@ -1,10 +1,7 @@
 // export const API = process.env.REACT_APP_URL;
 // export const API = 'http://192.168.0.201:3000/';
-<<<<<<< HEAD
-export const API = 'https://api-lib.applifyapps.com/';
-=======
 
 export const API = 'https://api-lib.applifyapps.com/';
 
-export const imageAWSURL = 'https://applify-library.s3.ap-southeast-1.amazonaws.com/admin/'
->>>>>>> remotes/origin/19thDec
+export const imageAWSURL =
+  'https://applify-library.s3.ap-southeast-1.amazonaws.com/admin/';
